73df655d93622b9a5b6bc986398a328170954f52
David Andersson <David Andersson>
2017-05-24 14:28:09 +0200

    Counter component, enzyme state+events, all tests

--- uppgift13/src/Counter.js
+++ uppgift13/src/Counter.js
@@ -6,9 +6,27 @@ class Counter extends Component {
 		this.state = {
 			number: 2
 		}
+		this.handleIncreaseClick = this.handleIncreaseClick.bind(this);
+		this.handleDecreaseClick = this.handleDecreaseClick.bind(this);
 	}
 	render() {
-		return <div>{this.state.number}</div>;
+		return <div>
+			{this.state.number}
+			<button className="increase" onClick={this.handleIncreaseClick}>Increase</button>
+			<button className="decrease" onClick={this.handleDecreaseClick}>Decrease</button>
+		</div>;
+	}
+	handleIncreaseClick(event) {
+		this.setState({
+			number: this.state.number + 1
+		});
+	}
+	handleDecreaseClick(event) {
+		if( this.state.number > 0 ) {
+			this.setState({
+				number: this.state.number - 1
+			});
+		}
 	}
 }
 
--- uppgift13/src/Counter.test.js
+++ uppgift13/src/Counter.test.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 describe('Counter component test suite', () => {
+	// Börja med en prototyp!
 	// initial value: 2
 	// om increase fungerar
 	// om decrease fungerar
 	// - om värdet är större än noll
 	// - värdet är noll eller mindre
+	
 	test('initial value: 2', () => {
 		let wrapper = shallow(<Counter />);
 		let actual = wrapper.state('number');
@@ -17,4 +19,40 @@ describe('Counter component test suite', () => {
 		let index = wrapper.html().indexOf(expected);
 		expect( index ).toBeGreaterThan( -1 );
 	})
+	
+	test('increase increases value', () => {
+		let wrapper = shallow(<Counter />);
+		wrapper.find('button.increase').simulate('click');
+		let actual = wrapper.state('number');
+		let expected = 3;
+		expect(actual).toBe(expected);
+	});
+	
+		test('decrease decreases value when over 0', () => {
+		let wrapper = shallow(<Counter />);
+		wrapper.find('button.decrease').simulate('click');
+		let actual = wrapper.state('number');
+		let expected = 1;
+		expect(actual).toBe(expected);
+	});
+	test('decrease when 0 or lower', () => {
+		let wrapper = shallow(<Counter />);
+		wrapper.setState({ number: 0 });
+		wrapper.find('button.decrease').simulate('click');
+		let actual = wrapper.state('number');
+		let expected = 0;
+		expect(actual).toBe(expected);
+	})
+	
 })
+
+
+
+
+
+
+
+
+
+
+
